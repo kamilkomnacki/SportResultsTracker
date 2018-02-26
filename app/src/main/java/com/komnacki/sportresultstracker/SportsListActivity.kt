045fde7f67eae3698f5e3adbabcfa2ad5403697b
Kamil Komnacki <kamilkomnacki@gmail.com>
2018-02-26 21:24:45 +0100

    Add ListView with FloatButton.

--- app/src/main/java/com/komnacki/sportresultstracker/SportsListActivity.kt
+++ app/src/main/java/com/komnacki/sportresultstracker/SportsListActivity.kt
@@ -3,10 +3,14 @@ package com.komnacki.sportresultstracker
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
 
 import kotlinx.android.synthetic.main.activity_sports_list.*
+import kotlinx.android.synthetic.main.content_sports_list.*
 
 class SportsListActivity : AppCompatActivity() {
+
+    var list = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +21,27 @@ class SportsListActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        list.add("Element 1")
+        list.add("Element 2")
+        list.add("Element 3")
+        list.add("Element 4")
+        list.add("Element 5")
+        list.add("Element 6")
+        list.add("Element 7")
+        list.add("Element 8")
+        list.add("Element 9")
+        list.add("Element 10")
+        list.add("Element 11")
+        list.add("Element 12")
+        list.add("Element 13")
+        list.add("Element 14")
+        list.add("Element 15")
+        list.add("Element 16")
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list)
+        lv_sports.adapter = adapter
+
     }
 
 }
