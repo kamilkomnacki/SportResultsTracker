e03763913d522ee27cd168cec42959531af1899c
Kamil Komnacki <kamilkomnacki@gmail.com>
2018-02-28 19:59:14 +0100

    Add layout to SportEditActivity.

--- app/src/main/java/com/komnacki/sportresultstracker/SportEditActivity.kt
+++ app/src/main/java/com/komnacki/sportresultstracker/SportEditActivity.kt
@@ -2,11 +2,19 @@ package com.komnacki.sportresultstracker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class SportEditActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sport_edit)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        menuInflater.inflate(R.menu.sport_edit_menu, menu)
+        return true
     }
 }
