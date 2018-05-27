1192f78d6ca2655a222aa02e48531b021b05e7d7
Kamil Komnacki <kamilkomnacki@gmail.com>
2018-05-27 17:23:18 +0200

    Set mast for time in RecordInputDialogFragment. Change time storage convention.

--- app/src/main/java/com/komnacki/sportresultstracker/RecordInputDialogFragment.kt
+++ app/src/main/java/com/komnacki/sportresultstracker/RecordInputDialogFragment.kt
@@ -5,7 +5,6 @@ import android.app.Dialog
 import android.app.DialogFragment
 import android.os.Bundle
 import android.text.Editable
-import android.util.Log
 import android.view.LayoutInflater
 import com.komnacki.sportresultstracker.database.Record
 import kotlinx.android.synthetic.main.alert_dialog_record_input.*
@@ -45,7 +44,6 @@ class RecordInputDialogFragment : DialogFragment() {
                         sv_alert_dialog_record_edit,
                         false)
 
-        //Wyświetlaj tylko te pola co są zaznaczone w danym sporcie
         val etTime = viewInflated.et_time_recordEdit
         val etDistance = viewInflated.et_distance_recordEdit
         val calendar = viewInflated.calendarEdit
@@ -54,7 +52,7 @@ class RecordInputDialogFragment : DialogFragment() {
             calendar.date = record.date!!.time
 
             if (!record.time!!.equals(Long.MIN_VALUE))
-                etTime.setText(record.time.toString())
+                etTime.setText(getTimeFromDatabaseLongValue(record.time!!))
 
             if (!record.distance!!.equals(Long.MIN_VALUE)) {
                 val distanceBigDecimal: BigDecimal = record.distance!!
@@ -73,28 +71,9 @@ class RecordInputDialogFragment : DialogFragment() {
             val offset = timeNow.get(Calendar.ZONE_OFFSET) + timeNow.get(Calendar.DST_OFFSET)
             val currentTime = (timeNow.timeInMillis + offset) % (24 * 60 * 60 * 1000)
 
-            val newDateMiliseconds = newDate.time + currentTime
-            date = java.util.Date(newDateMiliseconds)
+            val newDateMilliseconds = newDate.time + currentTime
+            date = java.util.Date(newDateMilliseconds)
         }
-
-
-//        val etTime_Listener = MaskedTextChangedListener(
-//                "##:##:##:###",
-//                true,
-//                etTime,
-//                null,
-//                object: MaskedTextChangedListener.ValueListener{
-//                    override fun onTextChanged(maskFilled: Boolean, extractedValue: String) {
-//                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//                    }
-//                }
-//        )
-
-        //COS TU NIE DZIALALO JAK PRZYSZEDLEM... @TODO("not implemented")
-        // etTime.addTextChangedListener(etTime_Listener)
-        //etTime.onFocusChangeListener = etTime_Listener
-        // et_time_recordEdit.addTextChangedListener(etTime_Listener)
-        // et_time_recordEdit.onFocusChangeListener = etTime_Listener
 
         alert.setView(viewInflated)
         alert.setTitle(title)
@@ -102,11 +81,11 @@ class RecordInputDialogFragment : DialogFragment() {
         alert.setPositiveButton(android.R.string.yes) { dialog, position ->
             record.date = date
 
+
             val etTimeRec = viewInflated.et_time_recordEdit.text
-            if (!fieldIsBlank(etTimeRec)) {
-                record.time = viewInflated.et_time_recordEdit.text.toString().toLong()
-                //@TODO("Obsłużyć maskę czasu")
-            } else
+            if (!fieldIsBlank(etTimeRec))
+                record.time = getTimeFromMaskedEditText(viewInflated.et_time_recordEdit.text.toString())
+            else
                 record.time = Long.MIN_VALUE
 
             val etDistanceRec = viewInflated.et_distance_recordEdit.text
@@ -125,6 +104,54 @@ class RecordInputDialogFragment : DialogFragment() {
         alert.setNegativeButton(android.R.string.no) { dialog, position -> dialog?.cancel() }
 
         return alert.create()
+    }
+
+    private fun getTimeFromMaskedEditText(stringValue: String): Long {
+        if (stringValue.isEmpty())
+            return Long.MIN_VALUE
+
+        var stringValueBuilder = StringBuilder(stringValue)
+
+        for ((index, s) in stringValueBuilder.withIndex()) {
+            if (!s.isDigit() && !s.equals(':'))
+                stringValueBuilder[index] = '0'
+        }
+
+        var time: String
+        time = stringValueBuilder.subSequence(0, 2).toString()
+
+        val minutes = stringValueBuilder.subSequence(3, 5).toString()
+        time += if (minutes.toLong() < 59) minutes else "59"
+
+        val seconds = stringValueBuilder.subSequence(6, 8).toString()
+        time += if (seconds.toLong() < 59) seconds else "59"
+
+        time += stringValueBuilder.subSequence(9, 12).toString()
+
+        return time.toLong()
+    }
+
+    private fun getTimeFromDatabaseLongValue(value: Long): String {
+        if ((value == Long.MIN_VALUE) || value <= 0L)
+            return ""
+
+        var output = StringBuilder()
+        val stringValue = StringBuilder(value.toString())
+
+        if (stringValue.length < 9) {
+            var len = stringValue.length
+            while (len < 9) {
+                output.append('0')
+                len++
+            }
+        }
+
+        output.append(stringValue)
+        output.insert(1, ":")
+        output.insert(4, ":")
+        output.insert(7, ":")
+
+        return output.toString()
     }
 
     private fun fieldIsBlank(text: Editable?): Boolean {
